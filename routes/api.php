73f378540c9a6cfda88035ac8eefee0d14673da9
sazan-barznji <sazanbarazenjiE@gmail.com>
2022-08-29 23:54:20 +0300

    commit

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('register', 'App\Http\Controllers\API\RegisterController@register');
 Route::post('login', 'App\Http\Controllers\API\RegisterController@login');
+// Route::apiResources(['user' => 'API\UserController']);
+// Route::apiResources(['user' , 'App\Http\Controllers\API\UserController']);
+Route::apiResource('user',UserController::class);
+
 
 // Route::middleware('auth:api')->group(function () {
     
